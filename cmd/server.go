fb98cba3c35ad88a05cedfc02e0c3f05d231bebe
chenyuteng <634024776@qq.com>
2022-10-27 15:28:36 +0800

    Add: handle os.Signal

--- cmd/server.go
+++ cmd/server.go
@@ -4,7 +4,10 @@ import (
 	"cd_platform/controller"
 	"cd_platform/ext"
 	"net/http"
+	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 )
 
 func main() {
@@ -13,7 +16,32 @@ func main() {
 	e := controller.InitController()
 
 	wg := sync.WaitGroup{}
-	wg.Add(1)
+	wg.Add(2)
 	go http.ListenAndServe(":8080", e)
+	go handleSignal()
 	wg.Wait()
+}
+
+func handleSignal() {
+	sigchan := make(chan os.Signal)
+
+	signal.Notify(sigchan)
+
+	s := <-sigchan
+
+	switch s {
+	case syscall.SIGQUIT:
+		clean()
+		os.Exit(-1)
+	case syscall.SIGKILL:
+		clean()
+		os.Exit(-1)
+	case syscall.SIGTERM:
+		clean()
+		os.Exit(-1)
+	}
+}
+
+func clean() {
+
 }
