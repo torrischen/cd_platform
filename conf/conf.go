c288ba11089f9fefd44ab6fa956dec1b94c4f7ea
nainaiguan <634024776@qq.com>
2022-11-21 10:05:36 +0800

    Bugfix: conf.conf

--- conf/conf.go
+++ conf/conf.go
@@ -17,7 +17,7 @@ var Conf Config
 
 func InitConf() {
 	flag.StringVar(&Conf.KubecfgDir, "kubeConfigDir", "./conf/kubecfg.yaml", "kubecfg")
-	flag.StringVar(&Conf.MysqlAddr, "mysqlAddr", "localhost:3306", "mysqladdr")
+	flag.StringVar(&Conf.MysqlAddr, "mysqlAddr", "192.168.3.51:3306", "mysqladdr")
 	flag.StringVar(&Conf.MysqlUser, "mysqlUser", "root", "mysqluser")
 	flag.StringVar(&Conf.MysqlPass, "mysqlPass", "123456", "mysqlpass")
 	flag.StringVar(&Conf.MysqlDb, "mysqlDbName", "cd", "mysqldb")
